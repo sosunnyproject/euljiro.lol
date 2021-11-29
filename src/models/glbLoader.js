33ae8a9e81b3f98dd10b4f1b4a732aa45bcf94b4
sosunnyproject <parksosun1103@gmail.com>
2021-11-29 13:26:17 +0900

    change model

--- src/models/glbLoader.js
+++ src/models/glbLoader.js
@@ -17,7 +17,7 @@ import ROBOT_CRANE from "../../assets/districtOne/Cranebot0.glb"
 import ROBOT_CHIP from "../../assets/districtOne/Arduinobot0.glb"
 
 // district 2
-import COFFEE_SPA from "../../assets/districtTwo/coffeeSpa2.glb"
+import COFFEE_SPA from "../../assets/districtTwo/Coffeecupsap27.glb"
 import TAPE from "../../assets/districtTwo/Tape19.glb"
 import BEAR from "../../assets/districtTwo/Bear28.glb"
 import FORK from "../../assets/districtTwo/fork.glb"
